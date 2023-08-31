39b2d60ea7e421e46d7d8e5c42ea7b3d24c45695
Þorvaldur Tumi <tumi@juni.is>
2023-08-31 14:19:05 +0000

    center frontpage

--- programs/src/routes/+page.svelte
+++ programs/src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1>Velkomi/ð/n/n</h1>
-<h2>Endilega skoðið ykkur um</h2>
-<h3>Hér er margt að finna</h3>
+<div>
+  <h1>Velkomi/ð/n/n</h1>
+  <h2>Endilega skoðið ykkur um</h2>
+  <h3>Hér er margt að finna</h3>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
