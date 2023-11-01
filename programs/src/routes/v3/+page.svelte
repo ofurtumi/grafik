7e02af176e8704485c3fa87b94944d5501cabb05
Þorvaldur Tumi <tumi@juni.is>
2023-11-01 14:49:59 +0000

    fjarlægja óþarfa hluti

--- programs/src/routes/v3/+page.svelte
+++ programs/src/routes/v3/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import * as THREE from "three";
-  import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+  import { Sigga } from "./sigga";
 
   let container: HTMLDivElement;
 
@@ -14,107 +14,57 @@
     down: false,
   };
 
-  const cubes = [];
-
   const createScene = () => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      container.offsetWidth / container.offsetHeight,
+      0.1,
+      1000
+    );
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(500, 500);
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
     container.appendChild(renderer.domElement);
 
-    const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load("/flip-tumi.jpg");
-
-    const loader = new OBJLoader();
-
-    // load a resource
-    loader.load(
-      // resource URL
-      "/avatar.obj",
-      // called when resource is loaded
-      function (object) {
-        object.traverse((child)=>{
-          if (child instanceof THREE.Mesh) {
-            child.material = new THREE.MeshStandardMaterial({
-            color: 0xFFF32F
-        })
-            console.log("hæ");
-          }
-        })
-
-        object.scale.set(0.01, 0.01, 0.01); // Scale by 0.1 in all directions
-
-        object.position.y = -1;
-        scene.add(object);
+    let sigga: THREE.Group<THREE.Object3DEventMap>;
+    Sigga({
+      func: (obj) => {
+        obj.scale.set(0.05, 0.05, 0.05);
+        obj.position.set(0, -1, -1);
+        sigga = obj;
+        scene.add(obj);
       },
-      // called when loading is in progresses
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-      },
-      // called when loading has errors
-      function (error) {
-        console.log("An error happened");
-      }
-    );
+    });
 
-    for (let i = 0; i < 6; ++i) {
-      const cube = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1.5, 1),
-        new THREE.MeshStandardMaterial({
-          map: texture,
-        })
-      );
-      cube.position.x = Math.sin(i) * 2;
-      cube.position.z = Math.cos(i) * 2;
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-      scene.add(cube);
-      cubes.push(cube);
-    }
-
-    const lightsource = new THREE.AmbientLight(0xff00ff, 2);
-    scene.add(lightsource);
-    const light = new THREE.Mesh(
-      new THREE.SphereGeometry(0.5),
-      new THREE.MeshPhongMaterial({
-        emissive: 0xff00ff,
-      })
-    );
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(5, 5, 5).normalize();
-  scene.add(directionalLight);
-
+    const light = new THREE.PointLight(0x444444, 10);
+    light.position.set(0, 2, 2);
+    const light_helper = new THREE.PointLightHelper(light);
     scene.add(light);
-
-    camera.position.z = 2;
-    camera.position.y = 2;
-    camera.rotation.x = -1;
+    scene.add(light_helper);
 
     function animate() {
       if (movement.left) {
-        lightsource.position.x -= 0.1;
-        light.position.x -= 0.1;
+        camera.position.x -= 0.1;
       }
       if (movement.right) {
-        lightsource.position.x += 0.1;
-        light.position.x += 0.1;
+        camera.position.x += 0.1;
       }
       if (movement.forward) {
-        lightsource.position.z -= 0.1;
-        light.position.z -= 0.1;
+        camera.position.z -= 0.1;
       }
       if (movement.back) {
-        lightsource.position.z += 0.1;
-        light.position.z += 0.1;
+        camera.position.z += 0.1;
       }
       if (movement.up) {
-        lightsource.position.y += 0.1;
-        light.position.y += 0.1;
+        camera.position.y += 0.1;
       }
       if (movement.down) {
-        lightsource.position.y -= 0.1;
-        light.position.y -= 0.1;
+        camera.position.y -= 0.1;
       }
 
       renderer.render(scene, camera);
@@ -147,3 +97,10 @@
 <div id="container" bind:this={container} />
 
 <svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} />
+
+<style>
+  #container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
