a62ae3adb1299cc96be86c369acab13e50c69651
Þorvaldur Tumi <tumi@juni.is>
2023-11-07 16:26:46 +0000

    update to remove ts error

--- programs/src/routes/v3/Centipede.ts
+++ programs/src/routes/v3/Centipede.ts
@@ -8,6 +8,7 @@ export interface CENTIPEDE {
   next_up?: boolean;
   length: number;
   spheres?: Mesh[];
+  color?: number;
 }
 export const Centipede = ({
   direction,
@@ -16,24 +17,26 @@ export const Centipede = ({
   head,
   tail,
   length,
+  color = 0x32ff00,
 }: CENTIPEDE) => {
   if (!tail) {
     tail = new Array(length - 1).fill(new Vector3(0, 0, 0));
   }
 
   const sphereGeometry = new SphereGeometry(0.4, 32, 32);
-  const sphereMaterial = new MeshPhongMaterial({ color: 0x32ff00 });
-  const spheres = new Array(length).fill(0).map((_) => {
+  const sphereMaterial = new MeshPhongMaterial({ color: color });
+  let spheres = new Array(length).fill(0).map((_) => {
     return new Mesh(sphereGeometry, sphereMaterial);
   });
 
   if (spheres.length > 0 && head) {
-    spheres[0].position.set(...head);
-    console.log(tail);
-    for (let i = 0; i < length - 2; i++) {
-      spheres[i + 1].position.set(...tail[i]);
+    let { x, y, z } = head;
+    spheres[0].position.set(x, y, z);
+    for (let i = 0; i < length - 1; i++) {
+      let { x, y, z } = tail[i];
+      spheres[i + 1].position.set(x, y, z);
     }
-  }
+  } else spheres = [];
 
   return {
     direction,
